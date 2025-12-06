1eda292b09c6b08be7f80be90a9875e55aac4e1b
Daniil Shevtsov <shevtsov200@gmail.com>
2025-12-06 07:57:23 +0100

    add second enemy

--- Game.cs
+++ Game.cs
@@ -1,12 +1,16 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 public partial class Game : Node2D
 {
 	private Player _player;
 
+	private List<Enemy> _enemies = new();
+
 	private Enemy _enemy;
+	private Enemy _enemy2;
 
 	private bool _isPlayerInSight = false;
 
@@ -18,45 +22,54 @@ public partial class Game : Node2D
 	{
 		_player = GetNode<Player>("Player");
 		_enemy = GetNode<Enemy>("Enemy");
+		_enemy2 = GetNode<Enemy>("Enemy2");
 		_bulletResource = GD.Load<PackedScene>("res://bullet.tscn");
 
-		SetCallbackSafe(_enemy._sightArea, (area2D) =>
-		{
-			area2D.BodyEntered += body =>
-			{
-				if (body is Player)
-				{
-					_isPlayerInSight = true;
-				}
-			};
-			area2D.BodyExited += body =>
-			{
-				if (body is Player)
-				{
-					_isPlayerInSight = false;
-				}
-			};
-			return true;
-		});
+		_enemies.Add(_enemy);
+		_enemies.Add(_enemy2);
 		
-		SetCallbackSafe(_enemy._runArea, (area2D) =>
+		_enemies.ForEach(enemy =>
 		{
-			area2D.BodyEntered += body =>
+			SetCallbackSafe(enemy._sightArea, (area2D) =>
 			{
-				if (body is Player)
+				area2D.BodyEntered += body =>
 				{
-					_isPlayerInRun = true;
-				}
-			};
-			area2D.BodyExited += body =>
+					if (body is Player)
+					{
+						_isPlayerInSight = true;
+					}
+				};
+				area2D.BodyExited += body =>
+				{
+					if (body is Player)
+					{
+						_isPlayerInSight = false;
+					}
+				};
+				return true;
+			});
+		
+			SetCallbackSafe(enemy._runArea, (area2D) =>
 			{
-				if (body is Player)
+				area2D.BodyEntered += body =>
 				{
-					_isPlayerInRun = false;
-				}
-			};
-			return true;
+					if (body is Player)
+					{
+						_isPlayerInRun = true;
+					}
+				};
+				area2D.BodyExited += body =>
+				{
+					if (body is Player)
+					{
+						_isPlayerInRun = false;
+					}
+				};
+				return true;
+			});
 		});
+
+		
 		
 		SetCallbackSafe(_player._damageArea, (area2D) =>
 		{
@@ -89,31 +102,33 @@ public partial class Game : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (_isPlayerInSight)
+		_enemies.ForEach((enemy) =>
 		{
-			_enemy.onPlayerDetected(_player.GlobalPosition);
-		}
+			if (_isPlayerInSight)
+			{
+				enemy.onPlayerDetected(_player.GlobalPosition);
+			}
 
-		if (_isPlayerInRun && !_isPlayerInSight)
-		{
-			_enemy.SetRunTarget(_player._frontMarker.GlobalPosition);
-		}
+			if (_isPlayerInRun && !_isPlayerInSight)
+			{
+				enemy.SetRunTarget(_player._frontMarker.GlobalPosition);
+			}
 
-		var isTimeToShoot = _enemy._timer.TimeLeft == 0f;
-		if (isTimeToShoot)
-		{
-			var bullet = (Bullet) _bulletResource.Instantiate().Duplicate();
-			var bulletSpawnPosition = _enemy._gun._endMarker.GlobalPosition;
-			var direction = (bulletSpawnPosition - _enemy.GlobalPosition).Normalized();
-			var bulletSpeed = 500f;
-			var bulletVelocity = direction * bulletSpeed;
+			var isTimeToShoot = enemy._timer.TimeLeft == 0f;
+			if (isTimeToShoot)
+			{
+				var bullet = (Bullet) _bulletResource.Instantiate().Duplicate();
+				var bulletSpawnPosition = enemy._gun._endMarker.GlobalPosition;
+				var direction = (bulletSpawnPosition - enemy.GlobalPosition).Normalized();
+				var bulletSpeed = 500f;
+				var bulletVelocity = direction * bulletSpeed;
 			
-			AddChild(bullet);
-			bullet.GlobalPosition = bulletSpawnPosition;
-			bullet.Rotation = bulletVelocity.Angle();
-			bullet.LinearVelocity = bulletVelocity;
-			_enemy._timer.Start();
-		}
-		
+				AddChild(bullet);
+				bullet.GlobalPosition = bulletSpawnPosition;
+				bullet.Rotation = bulletVelocity.Angle();
+				bullet.LinearVelocity = bulletVelocity;
+				enemy._timer.Start();
+			}
+		});
 	}
 }
